5dc6fa03f9b8a9a5e7236b007e86e4c0dc10ae52
hironomiu <hironomiu@gmail.com>
2022-09-12 10:05:35 +0900

    commited

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,36 @@
 import { render, screen } from '@testing-library/react'
 import App from '../App'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom/extend-expect'
+import 'cross-fetch/polyfill'
+import { setupServer } from 'msw/node'
+import { handlers } from '../mock'
+
+const server = setupServer(...handlers)
+
+beforeAll(() => {
+  server.listen()
+})
+
+afterEach(() => {
+  server.resetHandlers()
+})
+
+afterAll(() => {
+  server.close()
+})
 
 describe('App', () => {
   it('test', async () => {
     render(<App />)
+    expect(screen.getByText('Header')).toBeInTheDocument()
+    await userEvent.type(
+      screen.getByTestId('input-email'),
+      'hanako@example.com'
+    )
+    await userEvent.type(screen.getByTestId('input-password'), 'password')
+    await userEvent.click(screen.getByTestId('input-submit'))
+
+    expect(await screen.findByText('Main')).toBeInTheDocument()
   })
 })
--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,1 +1,9 @@
-export const handlers = {}
+import { rest } from 'msw'
+export const handlers = [
+  rest.post('http://127.0.0.1:4141/api/v1/auth/signin', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({ isSuccess: true, message: 'MSW mock return' })
+    )
+  }),
+]
