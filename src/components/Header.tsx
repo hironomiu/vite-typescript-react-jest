cb6b7069674b72ec41c76495cad93c3b3539d818
hironomiu <hironomiu@gmail.com>
2022-09-13 20:03:06 +0900

    commited

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { isSignInAtom } from '../recoil/global'
 
 const Header = () => {
+  const [isSignIn, setIsSignIn] = useRecoilState(isSignInAtom)
+  const handleClickSignOut = () => {
+    setIsSignIn(false)
+  }
+
   return (
     <header className="flex items-center justify-between px-4 h-16 bg-gray-200">
       <div>
         <h1 className="text-3xl font-bold">Header</h1>
       </div>
       <nav>
-        <ul className="flex">
+        <ul className="flex space-x-2">
           <li>hoge</li>
           <li>fuga</li>
+          {isSignIn ? (
+            <li onClick={handleClickSignOut} className="hover:cursor-pointer">
+              SignOut
+            </li>
+          ) : null}
         </ul>
       </nav>
     </header>
