becebe09cd5588f0cf41fcd5e5ab8ebc15bab095
hironomiu <hironomiu@gmail.com>
2022-09-15 05:59:24 +0900

    commited

---
+++ src/__tests__/Header.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import { RecoilRoot } from 'recoil'
+import Header from '../components/Header'
+import '@testing-library/jest-dom/extend-expect'
+describe('Header', () => {
+  it('test', async () => {
+    render(
+      <RecoilRoot>
+        <Header />
+      </RecoilRoot>
+    )
+
+    expect(screen.getByText('Header')).toBeInTheDocument()
+    expect(screen.queryByText('SignOut')).toBeNull()
+  })
+})
