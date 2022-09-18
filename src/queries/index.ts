6601ffdc4c5bfb847885ec85dd732dfc44dcd84d
hironomiu <hironomiu@gmail.com>
2022-09-18 18:39:21 +0900

    commited

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -3,7 +3,7 @@ import { VITE_API_URL } from '../config'
 
 export const fetchSignInPost = async (user: User) => {
   console.log(VITE_API_URL)
-  const response = await fetch(VITE_API_URL + '/api/v1/auth/signin', {
+  const response = await fetch(new URL('/api/v1/auth/signin', VITE_API_URL), {
     method: 'POST',
     mode: 'cors',
     cache: 'no-cache',
