f22703567b6b22ec25ed3519119cee714db2b3f7
hironomiu <hironomiu@gmail.com>
2022-10-01 15:41:37 +0900

    commited

--- src/__tests__/Question.test.tsx
+++ src/__tests__/Question.test.tsx
@@ -2,13 +2,19 @@ import { screen, render } from '@testing-library/react'
 import Question from '../components/Question'
 import '@testing-library/jest-dom/extend-expect'
 
-const id = 1
-const questionMessage = 'test1'
-
 describe('Question', () => {
   it('test', () => {
+    const id = 1
+    const questionMessage = 'test1'
     render(<Question id={id} questionMessage={questionMessage} />)
     expect(screen.getByText('Question1')).toBeInTheDocument()
     expect(screen.getByText('test1')).toBeInTheDocument()
+  })
+  it('全角', () => {
+    const id = 1
+    const questionMessage = 'テスト１'
+    render(<Question id={id} questionMessage={questionMessage} />)
+    expect(screen.getByText('Question1')).toBeInTheDocument()
+    expect(screen.getByText('テスト１')).toBeInTheDocument()
   })
 })
