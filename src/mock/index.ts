834c8ab94e0cac5008ca64d49261161dc3757235
hironomiu <hironomiu@gmail.com>
2022-10-09 10:28:00 +0900

    commited

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -1,22 +1,23 @@
 import { rest } from 'msw'
+import { VITE_API_URL } from '../config'
+
 export const handlers = [
-  rest.post(
-    'http://127.0.0.1:4141/api/v1/auth/signin',
-    async (req, res, ctx) => {
-      const json = await req.json()
-      if (json.email === 'hanako@example.com' && json.password === 'password') {
-        return res(
-          ctx.status(200),
-          ctx.json({ isSuccess: true, message: 'MSW mock return' })
-        )
-      }
+  // TODO new URLだとエラーになる
+  rest.post(VITE_API_URL + '/api/v1/auth/signin', async (req, res, ctx) => {
+    const json = await req.json()
+    if (json.email === 'hanako@example.com' && json.password === 'password') {
       return res(
         ctx.status(200),
-        ctx.json({ isSuccess: false, message: 'MSW mock error return' })
+        ctx.json({ isSuccess: true, message: 'MSW mock return' })
       )
     }
-  ),
-  rest.get('http://127.0.0.1:4141/api/v1/lessons', async (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json({ isSuccess: false, message: 'MSW mock error return' })
+    )
+  }),
+  // TODO new URLだとエラーになる
+  rest.get(VITE_API_URL + '/api/v1/lessons', async (req, res, ctx) => {
     return res(
       ctx.status(200),
       ctx.delay(500),
